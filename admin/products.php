5536a2570605606423bfd19cc48c24772485005c
Agnesa <aagnesarexhepi76@gmail.com>
2026-02-01 17:21:29 +0100

    Added products , for listing the projects

--- admin/products.php
+++ admin/products.php
@@ -23,15 +23,17 @@ $projects = $repo->getAllProjects();
             <th>Veprime</th>
         </tr>
         <?php foreach($projects as $p): ?>
-        <tr>
-            <td><img src="../uploads/<?php echo $p['image']; ?>" width="50"></td>
-            <td><?php echo $p['title']; ?></td>
-            <td><?php echo $p['location']; ?></td>
-            <td>
-                <a href="edit.php?id=<?php echo $p['id']; ?>">Edit</a> | 
-                <a href="products.php?del=<?php echo $p['id']; ?>" onclick="return confirm('Sigurt?')">Delete</a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
+<tr>
+    <td style="text-align: center;">
+        <img src="../uploads/<?php echo htmlspecialchars($p['image']); ?>" width="60" style="border-radius: 5px;">
+    </td>
+    <td><?php echo htmlspecialchars($p['title']); ?></td>
+    <td><?php echo htmlspecialchars($p['location']); ?></td>
+    <td><?php echo htmlspecialchars($p['created_by'] ?? 'Admin'); ?></td> <td>
+        <a href="edit.php?id=<?php echo $p['id']; ?>" style="color: blue; text-decoration: none;"><i class="fas fa-edit"></i> Edit</a> | 
+        <a href="products.php?del=<?php echo $p['id']; ?>" style="color: red; text-decoration: none;" onclick="return confirm('A jeni të sigurt që dëshironi ta fshini këtë projekt?')"><i class="fas fa-trash"></i> Delete</a>
+    </td>
+</tr>
+<?php endforeach; ?>
     </table>
 </div>
