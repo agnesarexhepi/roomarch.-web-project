6c45c3583b277bb5236b2b7123ab5ea939dd4f66
Agnesa <aagnesarexhepi76@gmail.com>
2026-02-01 17:05:52 +0100

    Added two methods about getting and removing projects from db

--- models/Project.php
+++ models/Project.php
@@ -14,11 +14,28 @@ class ProjectRepository {
     }
 
     // shton një projekt 
-    public function addProject($title, $location, $image, $description, $created_by) {
+   public function addProject($title, $location, $image, $description, $created_by) {
         // Përshtatja me kolonat: title, location, image, description, created_by
         $sql = "INSERT INTO projects (title, location, image, description, created_by) VALUES (?, ?, ?, ?, ?)";
         $stmt = $this->connection->prepare($sql);
         return $stmt->execute([$title, $location, $image, $description, $created_by]);
+    }
+
+    // --- METODAT E REJA PËR DASHBOARD ---
+
+    // Merr  nje projekt
+    public function getProjectById($id) {
+        $sql = "SELECT * FROM projects WHERE id = ?";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    //Fshin 
+    public function deleteProject($id) {
+        $sql = "DELETE FROM projects WHERE id = ?";
+        $stmt = $this->connection->prepare($sql);
+        return $stmt->execute([$id]);
     }
 }
 ?>
